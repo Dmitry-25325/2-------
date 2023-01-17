bce7bcc634fb7a438b854fccb3d164ca0fb71c69
Dmitry Strelchenko <dstrelchenko2017@gmail.com>
2023-01-17 19:05:04 +0200

    Commit

---
+++ main.py
@@ -0,0 +1,26 @@
+import threading
+import PyQt5
+from PyQt5.QtWidgets import QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import (
+        QWidget, 
+        QApplication
+        )
+from PyQt5.QtWidgets import QLabel
+from PyQt5.QtWidgets import QPushButton
+#Лошара
+#неправда
+
+
+
+
+
+
+
+
+
+
+
+
+
+
